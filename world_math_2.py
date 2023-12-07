1479242cf6a219176b2f622b77b6e108c767e708
ShamanPechenin <60006461+ShamanPechenin@users.noreply.github.com>
2023-12-07 23:10:06 +0200

    Added leaderboard

--- world_math_2.py
+++ world_math_2.py
@@ -1,5 +1,6 @@
 import random
 import time
+import json
 
 
 try:
@@ -17,7 +18,16 @@ def error():
     raise ValueError
 
 
+try:
+    with open("maths_leaderboard.json", 'r+') as file:
+        leaderboard = json.loads(file.read())
+except json.decoder.JSONDecodeError:
+    leaderboard = {"stats": []}
+except FileNotFoundError:
+    leaderboard = None
+
 LOGO = """
+--------------------------------------------------------------
 SQRT -(WORLD MATHS^2) v0.1 DEV BUILD
 (c) MAKSIMS, ARTJOMS, SERGEJS, KIRILS INC. ALL RIGHTS RESERVED
 --------------------------------------------------------------
@@ -26,30 +36,27 @@ SQRT -(WORLD MATHS^2) v0.1 DEV BUILD
 CURSOR_UP_ONE = '\x1b[1A'
 ERASE_LINE = '\x1b[2K'
 
-DIFFICULTY_LOOKUP = [  # Max numbers, Subtraction allowed, time to answer
-    [5, False, 120],
-    [10, False, 120],
-    [50, False, 120],
-    [100, False, 120],
-    [10, True, 20],
-    [50, True, 20],
-    [100, True, 20],
-    [1000, True, 60],
-    [1000, True, 30],
-    [1000, True, 20],
-    [1000, True, 10]
+DIFFICULTY_LOOKUP = [  # Max numbers, Subtraction allowed
+    [5, False],
+    [10, False],
+    [50, False],
+    [100, False],
+    [10, True],
+    [50, True],
+    [100, True],
+    [1000, True],
 ]
 
 print(LOGO)
 
-difficulty = input("ievadiet grūtības pakāpi no 0 līdz 10: ")
+difficulty = input("ievadiet grūtības pakāpi no 0 līdz 7: ")
 try:
     difficulty = int(difficulty)
-    max_nums, subtraction_allowed, time_to_answer = DIFFICULTY_LOOKUP[difficulty]
+    max_nums, subtraction_allowed = DIFFICULTY_LOOKUP[difficulty]
 except ValueError:
     error()
 
-print(f"Maksimālais skaitlis: {max_nums}; Atņemšana: {'ir' if subtraction_allowed else 'nav'}; laiks atbildēi: {time_to_answer} sekundes")
+print(f"Maksimālais skaitlis: {max_nums}; Atņemšana: {'ir' if subtraction_allowed else 'nav'}")
 
 n_right = 0
 answers_times = []
@@ -89,5 +96,19 @@ for i in range(10):
 if fancy_console:
     print(CURSOR_UP_ONE + ERASE_LINE, flush=True, end="\r")
 
+mean_time = sum(answers_times)/len(answers_times)
 print(f"{n_right}/{len(answers_times)}")
-print(f"Videjais laiks atbildem: {sum(answers_times)/len(answers_times):6f}±0.000005s")
+print(f"Videjais laiks atbildem: {mean_time:6f}±0.000005s")
+name = input("Ievadiet savu vārdu: ")
+
+if leaderboard is not None:
+    leaderboard["stats"].append({"name": name, "accuracy": n_right/len(answers_times), "time": mean_time})
+    with open("maths_leaderboard.json", 'w') as file:
+        file.write(json.dumps(leaderboard, indent=2))
+
+print("--------------------------------------------------------------\nRekordu tabula:")
+results = leaderboard["stats"]
+results = sorted(results, key=lambda x: x["time"])
+for i, player in enumerate(results[:10]):
+    print(f"{i}. vieta  {player['name']}:   {player['time']}")
+print("--------------------------------------------------------------")
